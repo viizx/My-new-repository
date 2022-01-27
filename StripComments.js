48dcc3bb96ace2f5c046bb06b12022ab107c88a5
viizx <viceradicaa@gmail.com>
2022-01-27 17:18:24 -0500

    Added solution for 'Strip Comments' kata

---
+++ StripComments.js
@@ -0,0 +1,16 @@
+function solution(input, markers) {
+    let arr = input.split("\n");
+  
+    let i = 0;
+  
+    arr.forEach(element => {
+      markers.forEach((i) => {
+        if (element.includes(i)) {
+          var b = arr.indexOf(element);
+          arr[b] = element.split(i).splice(0, 1).join().trim();
+        }
+      });
+     
+    });
+    return arr.join('\n')
+  }
