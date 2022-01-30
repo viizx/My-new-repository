63041e47a66fb30ea918d6de6e1d241bad220a22
viizx <viceradicaa@gmail.com>
2022-01-30 10:35:28 -0500

    Added solution for "Human Readable Duration Format" kata

--- StripComments.js
+++ StripComments.js
@@ -1,16 +1,15 @@
 function solution(input, markers) {
-    let arr = input.split("\n");
-  
-    let i = 0;
-  
-    arr.forEach(element => {
-      markers.forEach((i) => {
-        if (element.includes(i)) {
-          var b = arr.indexOf(element);
-          arr[b] = element.split(i).splice(0, 1).join().trim();
-        }
-      });
-     
+  let arr = input.split("\n");
+
+  let i = 0;
+
+  arr.forEach((element) => {
+    markers.forEach((i) => {
+      if (element.includes(i)) {
+        var b = arr.indexOf(element);
+        arr[b] = element.split(i).splice(0, 1).join().trim();
+      }
     });
-    return arr.join('\n')
-  }
+  });
+  return arr.join("\n");
+}
