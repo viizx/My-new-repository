f4951b15c7b27060fbd803243b0a0313835baa31
viizx <viceradicaa@gmail.com>
2022-04-03 14:51:37 -0400

    refactor

--- SequenceClassifier.js
+++ SequenceClassifier.js
@@ -38,6 +38,7 @@ function sequenceClassifier(arr) {
   }
 
   if (isConstant(arr)) return 5;
+  
   function isIncreasing(arr) {
     return arr.every(function (x, i) {
       return i === 0 || x >= arr[i - 1];
