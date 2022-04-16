fa0e41f3eab84658d452995affe77bac0969f7d6
viizx <viceradicaa@gmail.com>
2022-04-16 09:46:33 -0400

    Solution for Descending Order

---
+++ DescendingOrder.js
@@ -0,0 +1,5 @@
+function descendingOrder(n){
+    const arrOfDigits = Array.from(String(n), Number).sort(function(a, b){return b-a});
+    
+    return Number(arrOfDigits.sort(function(a, b){return b-a}).join(''))
+  }
